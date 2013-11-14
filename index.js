9ffab40b96b457d66b8a1e22a808d7164339c44f
Anthony Short <antshort@gmail.com>
2013-11-14 10:05:06 -0800

    Using requestAnimationFrame

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var ease = require('ease');
 var offset = require('offset');
+var raf = require('raf');
 
 function scrollTo(el, duration, easingType, scrollOffset) {
   scrollOffset = scrollOffset == null ? 0 : scrollOffset;
@@ -10,20 +11,18 @@ function scrollTo(el, duration, easingType, scrollOffset) {
   var fromY = document.body.scrollTop || html.scrollTop;
   var toY = fromY + offset(el).top + scrollOffset;
 
-  var updatePosition = function(){
+  function animate() {
+    if(stop) animate = function(){};
+    raf(animate);
     var now = Date.now();
     if (now - start >= duration) stop = true;
     var p = (now - start) / duration;
     var tick = Math.round(fromY + (toY - fromY) * easing(p));
     document.body.scrollTop = tick;
     html.scrollTop = tick;
-    setTimeout(function() {
-      if(stop) return;
-      updatePosition();
-    }, 10);
-  };
+  }
 
-  updatePosition();
-};
+  animate();
+}
 
 module.exports = scrollTo;
